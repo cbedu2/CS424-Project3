bc33b7e6b6fc3387f06c0235397bc1ae77eddaea
Will Toher <will.toher.0@gmail.com>
2019-04-14 17:54:51 -0500

    Add icons

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(AotClient)
 library(DT)
 library(leaflet)
+library(sp)
 
 source("viewAotData.R")
 
@@ -12,18 +13,29 @@ nodes <- reactive({
   getNodes()
 })
 
+starIcon <- makeIcon(
+  iconUrl = './assets/chicagostar25.png',
+  iconWidth = 20,
+  iconHeight = 25
+)
+
 shapeNodes <- function(nodedf) {
+  # Get node coordinates - this is a list of 1x2 vectors
   nodelist <- nodedf$location.geometry$coordinates
-  matrix(unlist(nodelist), ncol = 2, byrow = TRUE)
+  
+  # Unwrap list into a 2-column matrix
+  coords <- matrix(unlist(nodelist), ncol = 2, byrow = TRUE)
+  
+  SpatialPoints(coords)
 }
 
 server <- shinyServer(function(input, output) {
   output$table <- renderDataTable(nodes(),
                                   options = list(pageLength = 5))
   output$map <- renderLeaflet({
-    leaflet() %>%
+    leaflet(shapeNodes(nodes())) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
-      addMarkers(data=shapeNodes(nodes()))
+      addMarkers(icon=starIcon)
   })
 })
 
