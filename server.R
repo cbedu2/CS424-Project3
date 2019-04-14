8f42b00ec04e41a6722d372c91cb07609a122443
dylanvo21 <dvo7@uic.edu>
2019-04-14 15:31:00 -0500

    testing datatable

--- server.R
+++ server.R
@@ -1,6 +1,10 @@
-
+library(AotClient)
 server <- shinyServer(function(input, output) {
-  
+  output$table <- renderDataTable(ls.nodes(),
+                options = list(
+                  pageLength = 5
+                )
+                )
 })
 
 server
