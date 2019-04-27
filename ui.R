123d77239dc2a994ec40a648a43e91d8737a931d
cbedu2 <cbedu2@uic.edu>
2019-04-27 16:29:41 -0500

    reformat code

--- ui.R
+++ ui.R
@@ -23,77 +23,65 @@ placeholderImg <- renderImage({
 
 css = tags$style(
   ".body{
-    background-color: aliceblue;
+  background-color: aliceblue;
   }"
 )
 
 ui <- bootstrapPage(
-  tags$body(
-  css,
-  h2("The Air I Breathe"),
-  tags$div(id="mainpage",class="col-12",
-    fluidRow(
-      tags$div(
-        height="75%",
-        style="height:100%",
-        class="col-xs-6 col-md-3 col-lg-3",
-        h3("Interactive Map"),
-        leafletOutput("map",height=600),
-        actionButton("mapRoadViewr", "Road Map"),
-        actionButton("mapSataliteView", "Satellite "),
-        actionButton("mapTerainView", "Terain")
-      ),
-      tags$div(
-        height="75%",
-        style="height:100%",
-        class="col-xs-6 col-md-3 col-lg-3",
-        h3("Data Table"),
-        DT::dataTableOutput("table")
-      ),
-      tags$div(
-        id="compareNodes",
-        class="col-xs-6 col-md-6 col-lg-6",
-        tags$div(
-                 class="col-xs-6 col-md-3 col-lg-3",
-                 style="height:100%; border: 5px solid red; height: 100%; width:50%",
-                 h3("Node1"),
-                 placeholderImg
+    tags$body(
+    css,
+    h2("The Air I Breathe"),
+    tags$div(id="mainpage", class="col-12",
+        tags$div(class="col-xs-6 col-md-3 col-lg-3", style="height:100%",
+            h3("Interactive Map"),
+            leafletOutput("map",height=600),
+            actionButton("mapRoadView", "Road Map"),
+            actionButton("mapSatelliteView", "Satellite"),
+            actionButton("mapTerrainView", "Terrain")
         ),
-        tags$div(height="75%",
-                 class="col-xs-6 col-md-3 col-lg-3",
-                 style="height:100%; border: 5px solid red; height: 100%; width:50%",
-                 h3("Node1"),
-                 placeholderImg
+        tags$div(class="col-xs-6 col-md-3 col-lg-3", style="height:100%",
+            h3("Data Table"),
+            DT::dataTableOutput("table")
+        ),
+        tags$div(id="compareNodes",class="col-xs-12 col-md-6 col-lg-6",
+            tags$div(class="col-xs-6 col-md-3 col-lg-3", style="height:100%; border: 5px solid red;width:50%",
+            h3("Node1"),
+            placeholderImg
+         ),
+        tags$div(class="col-xs-6 col-md-3 col-lg-3",style="height:100%; border: 5px solid red;width:50%",
+            h3("Node2"),
+            placeholderImg
         ),
         tags$div(
-          tags$p("select timeframe to view"),
-          actionButton("nowTimeView", "Now "),
-          actionButton("hoursTimeView", " Last 24 Hours"),
-          actionButton("daysTimeView", "Last 7 Days")
+            tags$p("select timeframe to view"),
+            actionButton("nowTimeView", "Now "),
+            actionButton("hoursTimeView", " Last 24 Hours"),
+            actionButton("daysTimeView", "Last 7 Days")
         )
-      )
-    ),
-    fluidRow(
-      tags$div(
-        class="col-md-12 col-lg-12 col-xs-4",
-        tags$div(
-          tags$p("Filter Options"),
-          actionButton("so2Filter", "SO2"),
-          actionButton("h2sFilter", "H2S"),
-          actionButton("o3Filter", "O3"),
-          actionButton("no2Filter", "NO2"),
-          actionButton("coFilter", "CO"),
-          actionButton("pm2Filter", "PM2.5"),
-          actionButton("pm10Filer", "PM10")
         ),
-        tags$p(
-          "this dashboard was created by Dylan Vo, Wilfried Bedu, William Toher. It uses data from",
-          tags$a(href="https://darksky.net/dev","Darksky"),
-          "and",
-          tags$a(href="#","things")
+        tags$div(class="col-md-12 col-lg-12 col-xs-4",
+            tags$div(
+                tags$p("Filter Options"),
+                actionButton("so2Filter", "SO2"),
+                actionButton("h2sFilter", "H2S"),
+                actionButton("o3Filter", "O3"),
+                actionButton("no2Filter", "NO2"),
+                actionButton("coFilter", "CO"),
+                actionButton("pm2Filter", "PM2.5"),
+                actionButton("pm10Filer", "PM10"),
+                actionButton("tempFilter", "Temperature"),
+                actionButton("lightFilter", "Light Intensity"),
+                actionButton("humFilter", "Humidity")
+            ),
+            tags$p(
+                "this dashboard was created by Dylan Vo, Wilfried Bedu, William Toher. It uses data from",
+                tags$a(href="https://darksky.net/dev","Darksky"),
+                "and",
+                tags$a(href="#","things"),
+                "more information available at our",
+                tags$a(href="https://cbedu2.github.io/CS424/Projects/Project3/index.html","Project Description page")
+            )
         )
-      )
     )
-  )
-))
+  ))
 ui
